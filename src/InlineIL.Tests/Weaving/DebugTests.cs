afbf8e40e36345a96fa13456b1346563036df6e6
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2019-10-23 22:16:09 +0200

    Fix additional references being added incorrectly
    A reference to netstandard2.1 was added when weaving a netstandard2.0
    assembly with the .NET Core 3.0 SDK.

--- src/InlineIL.Tests/Weaving/DebugTests.cs
+++ src/InlineIL.Tests/Weaving/DebugTests.cs
@@ -1,9 +1,11 @@
+extern alias standard;
 using System.Linq;
 using InlineIL.Fody.Processing;
 using InlineIL.Fody.Support;
 using InlineIL.Tests.AssemblyToProcess;
 using InlineIL.Tests.Support;
 using Mono.Cecil;
+using standard::InlineIL.Tests.StandardAssemblyToProcess;
 
 namespace InlineIL.Tests.Weaving
 {
@@ -12,9 +14,9 @@ namespace InlineIL.Tests.Weaving
         [DebugTest]
         public void SingleMethod()
         {
-            var assemblyPath = FixtureHelper.IsolateAssembly<AssemblyToProcessReference>();
+            var assemblyPath = FixtureHelper.IsolateAssembly<StandardAssemblyToProcessReference>();
             var type = typeof(MethodRefTestCases);
-            var methodName = nameof(MethodRefTestCases.ReturnType);
+            var methodName = nameof(MethodRefTestCases.ReturnMethodHandle);
 
             using var module = ModuleDefinition.ReadModule(assemblyPath);
             var weavingContext = new ModuleWeavingContext(module, new WeaverConfig(null, module));
