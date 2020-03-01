a278a4eeb4d8aad57a4ea526af45377858d49710
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2020-03-01 16:12:05 +0100

    Add explicit #nullable enable in T4 because of auto-generated code

--- src/InlineIL/IL.Emit.cs
+++ src/InlineIL/IL.Emit.cs
@@ -4,6 +4,8 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+#nullable enable
+
 using System.Diagnostics.CodeAnalysis;
 
 namespace InlineIL
