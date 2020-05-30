6fd0de00c5b6c02488d2b3c4d2675a71dbdaa4a4
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2020-05-30 23:46:47 +0200

    Add debug test to view sequence point mapping

--- src/InlineIL.Tests/Weaving/DebugTests.cs
+++ src/InlineIL.Tests/Weaving/DebugTests.cs
@@ -1,16 +1,28 @@
 extern alias standard;
+using System;
+using System.IO;
 using System.Linq;
+using System.Text;
 using InlineIL.Fody.Processing;
 using InlineIL.Fody.Support;
 using InlineIL.Tests.AssemblyToProcess;
 using InlineIL.Tests.Support;
 using Mono.Cecil;
+using Mono.Cecil.Cil;
 using standard::InlineIL.Tests.StandardAssemblyToProcess;
+using Xunit.Abstractions;
 
 namespace InlineIL.Tests.Weaving
 {
     public class DebugTests
     {
+        private readonly ITestOutputHelper _output;
+
+        public DebugTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
         [DebugTest]
         public void SingleMethod()
         {
@@ -25,6 +37,66 @@ namespace InlineIL.Tests.Weaving
             var methodDef = typeDef.Methods.Single(i => i.Name == methodName);
 
             new MethodWeaver(weavingContext, methodDef, NoOpLogger.Instance).Process();
+        }
+
+        [DebugTest]
+        public void SequencePoints()
+        {
+            using var assemblyStream = File.OpenRead(typeof(DebugTests).Assembly.Location);
+            using var symbolsStream = File.OpenRead(Path.ChangeExtension(typeof(DebugTests).Assembly.Location, ".pdb"));
+
+            var readerParams = new ReaderParameters
+            {
+                ReadSymbols = true,
+                SymbolStream = symbolsStream,
+                SymbolReaderProvider = new PortablePdbReaderProvider()
+            };
+
+            var assembly = AssemblyDefinition.ReadAssembly(assemblyStream, readerParams);
+
+            var type = assembly.MainModule.GetType(typeof(DebugTests).FullName);
+            var method = type.Methods.Single(i => i.Name == nameof(SequencePoints));
+
+            var sequencePoints = method.DebugInformation.SequencePoints.ToDictionary(i => i.Offset);
+            string[]? sourceLines = null;
+
+            foreach (var instruction in method.Body.Instructions)
+            {
+                if (sequencePoints.TryGetValue(instruction.Offset, out var sequencePoint))
+                {
+                    _output.WriteLine("");
+
+                    if (sequencePoint.IsHidden)
+                    {
+                        _output.WriteLine("// Hidden");
+                    }
+                    else
+                    {
+                        sourceLines ??= File.ReadAllLines(sequencePoint.Document.Url, Encoding.UTF8);
+
+                        var text = sequencePoint.EndLine == sequencePoint.StartLine
+                            ? sourceLines[sequencePoint.StartLine - 1].Substring(sequencePoint.StartColumn - 1, sequencePoint.EndColumn - sequencePoint.StartColumn)
+                            : sourceLines[sequencePoint.StartLine - 1].Substring(sequencePoint.StartColumn - 1);
+
+                        _output.WriteLine($"// {sequencePoint.StartLine}:{sequencePoint.StartColumn} - {sequencePoint.EndLine}:{sequencePoint.EndColumn} - {text}");
+                    }
+                }
+
+                _output.WriteLine(instruction.ToString());
+            }
+
+            // Examples
+            GC.KeepAlive(0);
+            GC.KeepAlive(1);
+            GC.KeepAlive(2);
+            {
+                GC.KeepAlive(3);
+                GC.KeepAlive(4);
+                GC.KeepAlive(5);
+            }
+            GC.KeepAlive(6);
+            GC.KeepAlive(7);
+            GC.KeepAlive(8);
         }
     }
 }
