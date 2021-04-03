bac5ffdc17fe7705523837f5c264ae08a40200e9
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2021-04-03 16:57:15 +0200

    Change the way the no opcode is handled

--- src/InlineIL/IL.Emit.cs
+++ src/InlineIL/IL.Emit.cs
@@ -1848,17 +1848,6 @@ namespace InlineIL
                 => IL.Throw();
 
             /// <summary>
-            /// <c>no.</c> - Indicates that the subsequent instruction need not perform the specified fault check when it is executed. Currently not implemented in the CLR.
-            /// <para>0x01 = <c>typecheck</c> (<c>castclass</c>, <c>unbox</c>, <c>ldelema</c>, <c>stelem.*</c>)</para>
-            /// <para>0x02 = <c>rangecheck</c> (<c>ldelem.*</c>, <c>ldelema</c>, <c>stelem.*</c>)</para>
-            /// <para>0x04 = <c>nullcheck</c> (<c>ldfld</c>, <c>stfld</c>, <c>callvirt</c>, <c>ldvirtftn</c>, <c>ldelem.*</c>, <c>stelem.*</c>, <c>ldelema</c>)</para>
-            /// <para>This is a prefix instruction.</para>
-            /// </summary>
-            /// <param name="operand">The operand.</param>
-            public static void No(byte operand)
-                => IL.Throw();
-
-            /// <summary>
             /// <c>rethrow</c> - Rethrows the current exception.
             /// <para>Stack Transition: none</para>
             /// </summary>
