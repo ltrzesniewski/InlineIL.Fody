bb44d46d705620ed3e1e10eee7c69b828d44fc2c
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2021-07-19 20:36:04 +0200

    Don't suppress analyzers in .editorconfig files
    It just doesn't feel like the right place to do so.

--- src/InlineIL.Fody/Properties/AssemblyInfo.cs
+++ src/InlineIL.Fody/Properties/AssemblyInfo.cs
@@ -1,3 +1,6 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("InlineIL.Tests")]
+
+[assembly: SuppressMessage("Microsoft.Design", "CA1014")] // Mark assemblies with CLSCompliantAttribute
---
+++ src/InlineIL/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Microsoft.Design", "CA1014")] // Mark assemblies with CLSCompliantAttribute
+[assembly: SuppressMessage("Microsoft.Design", "CA1028")] // Enum storage should be Int32
+[assembly: SuppressMessage("Microsoft.Design", "CA1065")] // Do not raise exceptions in unexpected locations
+[assembly: SuppressMessage("Performance", "CA1822")] // Mark members as static
+[assembly: SuppressMessage("Usage", "CA2225")] // Operator overloads have named alternates
