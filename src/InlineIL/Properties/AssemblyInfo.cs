7622e9ff38aa1e30f23ab3375321e764666d661c
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2021-07-30 18:34:11 +0200

    Suppress CA1801

--- src/InlineIL/Properties/AssemblyInfo.cs
+++ src/InlineIL/Properties/AssemblyInfo.cs
@@ -3,5 +3,6 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("Microsoft.Design", "CA1014")] // Mark assemblies with CLSCompliantAttribute
 [assembly: SuppressMessage("Microsoft.Design", "CA1028")] // Enum storage should be Int32
 [assembly: SuppressMessage("Microsoft.Design", "CA1065")] // Do not raise exceptions in unexpected locations
+[assembly: SuppressMessage("Usage", "CA1801")] // Review unused parameters
 [assembly: SuppressMessage("Performance", "CA1822")] // Mark members as static
 [assembly: SuppressMessage("Usage", "CA2225")] // Operator overloads have named alternates
