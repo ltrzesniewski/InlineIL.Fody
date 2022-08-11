ce1fdbd44ee5d6c1e85aed1391021fb26333bbdb
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2022-08-11 21:52:26 +0200

    Fix in-solution weaver due to Fody update
    Fody: Cannot load/use weaver InlineIL.Examples.InSolutionWeaver.InlineILInSolution. Weavers must inherit from BaseModuleWeaver which exists in the FodyHelpers nuget package.

--- src/InlineIL.Examples.InSolutionWeaver/InlineILInSolution.cs
+++ src/InlineIL.Examples.InSolutionWeaver/InlineILInSolution.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
 using Fody;
 using InlineIL.Fody;
 using Mono.Cecil;
@@ -8,14 +11,32 @@ namespace InlineIL.Examples.InSolutionWeaver;
 
 [SuppressMessage("ReSharper", "UnusedType.Global")]
 [SuppressMessage("ReSharper", "UnusedMember.Global")]
-public class InlineILInSolution : ModuleWeaver
+public class InlineILInSolution : BaseModuleWeaver
 {
     // This project enables in-solution weaving
     // https://github.com/Fody/Home/blob/master/pages/in-solution-weaving.md
+
+    private readonly ModuleWeaver _moduleWeaver = new();
 
     static InlineILInSolution()
     {
         GC.KeepAlive(typeof(ModuleDefinition));
         GC.KeepAlive(typeof(WeavingException));
     }
+
+    public override void Execute()
+    {
+        var properties = typeof(BaseModuleWeaver).GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                                                 .Where(p => p.CanRead && p.CanWrite);
+
+        foreach (var property in properties)
+            property.SetValue(_moduleWeaver, property.GetValue(this));
+
+        _moduleWeaver.Execute();
+    }
+
+    public override IEnumerable<string> GetAssembliesForScanning()
+        => _moduleWeaver.GetAssembliesForScanning();
+
+    public override bool ShouldCleanReference => _moduleWeaver.ShouldCleanReference;
 }
