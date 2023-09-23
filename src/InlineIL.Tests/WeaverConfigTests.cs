8fa9f35ea7d74733020d435cbefd2d208e860ab0
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2023-09-23 13:50:28 +0200

    Use raw strings

--- src/InlineIL.Tests/WeaverConfigTests.cs
+++ src/InlineIL.Tests/WeaverConfigTests.cs
@@ -2,6 +2,7 @@ using System.Xml.Linq;
 using Fody;
 using InlineIL.Fody.Support;
 using InlineIL.Tests.Support;
+using JetBrains.Annotations;
 using Xunit;
 
 namespace InlineIL.Tests;
@@ -11,44 +12,72 @@ public class WeaverConfigOptionsTests
     [Fact]
     public void should_parse_empty_config()
     {
-        Parse("<InlineIL />");
+        Parse(
+            """
+            <InlineIL />
+            """
+        );
     }
 
     [Fact]
     public void should_throw_on_unknown_attribute()
     {
-        AssertInvalid(@"<InlineIL Foo=""Bar""/>");
+        AssertInvalid(
+            """
+            <InlineIL Foo="Bar" />
+            """
+        );
     }
 
     [Fact]
     public void should_throw_on_unknown_element()
     {
-        AssertInvalid(@"<InlineIL><Foo/></InlineIL>");
+        AssertInvalid(
+            """
+            <InlineIL>
+                <Foo/>
+            </InlineIL>
+            """
+        );
     }
 
     [Fact]
     public void should_parse_SequencePoints()
     {
-        var config = Parse(@"<InlineIL SequencePoints=""True""/>");
+        var config = Parse(
+            """
+            <InlineIL SequencePoints="True" />
+            """
+        );
+
         config.SequencePoints.ShouldEqual(WeaverConfigOptions.SequencePointsBehavior.True);
     }
 
     [Fact]
     public void should_parse_Warnings()
     {
-        var config = Parse(@"<InlineIL Warnings=""Ignore""/>");
+        var config = Parse(
+            """
+            <InlineIL Warnings="Ignore" />
+            """
+        );
+
         config.Warnings.ShouldEqual(WeaverConfigOptions.WarningsBehavior.Ignore);
     }
 
     [Fact]
     public void should_throw_on_invalid_enum_value()
     {
-        AssertInvalid(@"<InlineIL SequencePoints=""Foo""/>");
+        AssertInvalid(
+            """
+            <InlineIL SequencePoints="Foo" />
+            """
+        );
     }
 
-    private static WeaverConfigOptions Parse(string xml)
-        => new WeaverConfigOptions(XElement.Parse(xml));
+    private static WeaverConfigOptions Parse([LanguageInjection(InjectedLanguage.XML)] string xml)
+        => new(XElement.Parse(xml));
 
-    private static void AssertInvalid(string xml)
+    private static void AssertInvalid([LanguageInjection(InjectedLanguage.XML)] string xml)
         => Assert.ThrowsAny<WeavingException>(() => new WeaverConfigOptions(XElement.Parse(xml)));
 }
